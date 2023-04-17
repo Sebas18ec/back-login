b538cb0be89794b4bff103a61f67c690b18bf7a4
Sebas18ec <sebasbelal@gmail.com>
2023-04-17 13:46:30 -0500

    Cambios

--- Backend-api/Controllers/ControladorAPIController.cs
+++ Backend-api/Controllers/ControladorAPIController.cs
@@ -17,38 +17,7 @@ namespace Backend_api.Controllers
     [ApiController]
     public class ControladorAPIController : ControllerBase
     {
-        // GET: api/ControladorAPI
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/ControladorAPI/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/ControladorAPI
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/ControladorAPI/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ControladorAPI/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-
+        
         [HttpGet]
         [Route("api/v1/emisores")]
         public async Task<ActionResult<List<Emisor>>> GetEmisoresAsync()
@@ -91,110 +60,9 @@ namespace Backend_api.Controllers
                 return BadRequest();
             }
 
-            /*_httpClient.BaseAddress = new Uri("http://apiservicios.ecuasolmovsa.com:3009");
-            
 
 
-            var response = await _httpClient.GetAsync($"/api/Usuarios?usuario={usuario}&password={password}");
-            if (response.IsSuccessStatusCode)
-            {
-                var content = await response.Content.ReadAsStringAsync();
-                return Ok(content);
-            }
-            else
-            {
-                return BadRequest();
-            }
         }
-        
-        try
-        {
-            var requestUrl = $"http://apiservicios.ecuasolmovsa.com:3009/api/Usuarios?usuario={HttpUtility.UrlEncode(login.Usuario)}&password={HttpUtility.UrlEncode(login.Contrasena)}";
-
-            var response = await _httpClient.GetAsync(requestUrl);
-
-            if (response.IsSuccessStatusCode)
-            {
-                var responseString = await response.Content.ReadAsStringAsync();
-                var responseJson = JObject.Parse(responseString);
-
-                bool success = (bool)responseJson["success"];
-                if (success)
-                {
-                    // Lógica para cuando el login es exitoso
-                    return Ok(new { success = true });
-                }
-                else
-                {
-                    string error = (string)responseJson["error"];
-                    // Lógica para cuando el login falla
-                    return Unauthorized(new { success = false, error = error });
-                }
-            }
-            else
-            {
-                // Lógica para manejar el error de la llamada a la API
-                var errorContent = await response.Content.ReadAsStringAsync();
-                return BadRequest($"Error al llamar a la API de autenticación. Detalles del error: {errorContent}");
-            }
-        }
-        catch (Exception ex)
-        {
-            return BadRequest($"Ocurrió un error al realizar la solicitud. Detalles del error: {ex.Message}");
-        }*/
-        }
-
-
-
-
-        /*[HttpPost("login")]
-        public async Task<ActionResult> Login([FromBody] LoginModel login)
-        {
-            try
-            {
-                var requestUrl = $"http://apiservicios.ecuasolmovsa.com:3009/api/Usuarios?usuario={login.Usuario}&password={login.Contrasena}";
-                var response = await _httpClient.GetAsync(requestUrl);
-                //var response = await _httpClient.PostAsJsonAsync(requestUrl, null);
-                response.EnsureSuccessStatusCode();
-
-                var content = await response.Content.ReadAsStringAsync();
-                var result = JsonConvert.DeserializeObject<LoginResult>(content);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var responseString = await response.Content.ReadAsStringAsync();
-                    var responseJson = JObject.Parse(responseString);
-
-                    bool success = (bool)responseJson["success"];
-                    if (success)
-                    {
-                        // Lógica para cuando el login es exitoso
-                        return Ok(new { success = true });
-                    }
-                    else
-                    {
-                        string error = (string)responseJson["error"];
-                        // Lógica para cuando el login falla
-                        return Unauthorized(new { success = false, error = error });
-                    }
-                }
-                else
-                {
-                    // Lógica para manejar el error de la llamada a la API
-                    var errorContent = await response.Content.ReadAsStringAsync();
-                    return BadRequest($"Error al llamar a la API de autenticación. Detalles del error: {errorContent}");
-                }
-
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"Ocurrió un error al realizar la solicitud. Detalles del error: {ex.Message}");
-            }
-        }
-
-        */
-
-
     }
     
 }
